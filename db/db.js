ecd4db4db96e23ecbc7591f01f807f3bcfe0137a
Juan Manuel Zappalorto <85179583+jzappalorto@users.noreply.github.com>
2025-03-26 20:20:07 -0300

    correccion del archivo de datos

--- db/db.js
+++ db/db.js
@@ -1,14 +1,11 @@
 const NeDB = require("nedb");
 const path = require("path");
-
-// Verifica si estamos en producción (Railway)
-const isProduction = process.env.NODE_ENV === 'production';
-//const isProduction = false;
+const config = require("../config/config")
 
 // Si estamos en producción, usamos el volumen persistente (/mnt/data)
-const dbPath = isProduction
-  ? path.join('/app/data', 'usuarios.db')  // Ruta en producción
-  : path.join(__dirname, "../basededatos", 'usuarios.db');  // Ruta local en desarrollo
+const dbPath = path.join(config.dataFolder, "usuarios.db"); // Asigna correctamente el archivo
+
+path.join(config.dataFolder, 'usuarios.db');
 
 // Base de datos de usuarios (solo para login)
 const usersDB = new NeDB({
