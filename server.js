84ef6313f7ccbb5da0031a0b8409a00632d70686
Juan Manuel Zappalorto <85179583+jzappalorto@users.noreply.github.com>
2025-03-26 22:09:09 -0300

    Push

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const config = require("./config/config");
 const loginRoutes = require("./routes/login");
 const legajosRoutes = require("./routes/legajos");
 const backupDataRoutes = require("./routes/backupData");
+const restoreRoutes = require("./routes/restore");
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/login", loginRoutes);
 app.use("/legajos", legajosRoutes);
 app.use("/backupData", backupDataRoutes);
+app.use("/restore", restoreRoutes);
 
 // Iniciar servidor
 const PORT = config.port || 5000;
